2fa7b3eaaf97e40fba3bc1a1f1d2ca766ae90866
LucaBenzi <benz14@hotmail.it>
2025-03-31 08:44:11 +0200

    added readme

--- esempio.c
+++ esempio.c
@@ -5,7 +5,7 @@ int main(int argc, char* argv[])
 	if(argc >= 2)
 		printf("Ciao GIT dalla %sCIIN", argv[1]);
 	else
-		printf("saluto tutti");
+		printf("saluto tutto il fermi");
 		
 	return 0;
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Programma incredibile della 5CIIN #
+
+## primo test di git ##
+
+abbiamo committato per ore e ore
