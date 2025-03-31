372ea84a2fadb9b4703cea605dd6b85e691815b1
LucaBenzi <benz14@hotmail.it>
2025-03-31 08:37:29 +0200

    ora saluto la classe che viene passata da terminale. Aggiunto file gitignore

--- esempio.c
+++ esempio.c
@@ -2,10 +2,11 @@
 
 int main(int argc, char* argv[])
 {
-	int classe;
-	printf("inserire classe: ");
-	scanf("%d", &classe);
-	printf("Ciao GIT dalla %dCIIN", classe);
+	if(argc >= 2)
+		printf("Ciao GIT dalla %sCIIN", argv[1]);
+	else
+		printf("non saluto alcuna classe");
+		
 	return 0;
 }
 
